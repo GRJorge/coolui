06f912de45ea42b9cf2fb9def778e8630546909f
axdevil <deb.devel@outlook.es>
2024-01-09 20:09:01 -0600

    Componente de informacion basica

---
+++ client/src/app/components/sign-up/basic/basic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'basic',
+  standalone: true,
+  imports: [],
+  templateUrl: './basic.component.html',
+  styleUrl: './basic.component.css'
+})
+export class BasicComponent {
+
+}

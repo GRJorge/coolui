2e468e42d452aba73eea06fa6a66e5cafb49e39e
axdevil <deb.devel@outlook.es>
2024-01-09 22:19:13 -0600

    Componente para nombre de usuario

--- client/src/app/components/sign-up/sign-up.component.ts
+++ client/src/app/components/sign-up/sign-up.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BasicComponent } from './basic/basic.component';
+import { UsernameComponent } from './username/username.component';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [BasicComponent],
+  imports: [BasicComponent, UsernameComponent],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })

04d7530c5d26fe4f212855070d7be1a6c2a61019
axdevil <deb.devel@outlook.es>
2024-01-13 19:49:52 -0600

    Formulario reactivo

--- client/src/app/components/sign-up/basic/basic.component.ts
+++ client/src/app/components/sign-up/basic/basic.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'basic',
-  standalone: true,
-  imports: [],
-  templateUrl: './basic.component.html',
-  styleUrl: './basic.component.css'
+    selector: 'basic',
+    standalone: true,
+    imports: [ReactiveFormsModule],
+    templateUrl: './basic.component.html',
+    styleUrl: './basic.component.css',
 })
 export class BasicComponent {
-
+    formGroup = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(4)]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+        confirmPassword: new FormControl('', Validators.required),
+    });
 }

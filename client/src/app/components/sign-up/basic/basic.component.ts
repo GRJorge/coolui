67b823d0602dcf7d9382a42774e5cd7438cfb329
axdevil <deb.devel@outlook.es>
2024-01-17 18:42:26 -0600

    Formulario reactivo y muestra de errores y alertas

--- client/src/app/components/sign-up/basic/basic.component.ts
+++ client/src/app/components/sign-up/basic/basic.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NotificationComponent } from '../../notification/notification.component';
 
 @Component({
     selector: 'basic',
     standalone: true,
-    imports: [ReactiveFormsModule],
+    imports: [NotificationComponent, ReactiveFormsModule],
     templateUrl: './basic.component.html',
     styleUrl: './basic.component.css',
 })
 export class BasicComponent {
+    passwordModel?: string;
+    confirmPasswordModel?: string;
+
     formGroup = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(4)]),
         password: new FormControl('', [Validators.required, Validators.minLength(6)]),
         confirmPassword: new FormControl('', Validators.required),
     });
+
+    getControl(name: string): AbstractControl {
+        return this.formGroup.get(name)!;
+    }
+    getControlError(name: string, error: string): boolean {
+        return this.formGroup.get(name)!.errors?.[error];
+    }
 }

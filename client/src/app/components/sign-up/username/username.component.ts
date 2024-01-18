63385583e1a93573bd604d94b82c83a48e168755
axdevil <deb.devel@outlook.es>
2024-01-17 19:13:29 -0600

    Formulario reactivo y muestra de errores/alertas

--- client/src/app/components/sign-up/username/username.component.ts
+++ client/src/app/components/sign-up/username/username.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NotificationComponent } from '../../notification/notification.component';
 
 @Component({
-  selector: 'username-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './username.component.html',
-  styleUrl: './username.component.css'
+    selector: 'username-form',
+    standalone: true,
+    imports: [NotificationComponent, ReactiveFormsModule],
+    templateUrl: './username.component.html',
+    styleUrl: './username.component.css',
 })
 export class UsernameComponent {
-
+    usernameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
 }

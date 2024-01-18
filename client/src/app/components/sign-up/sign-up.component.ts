53796526365d94a3901336f6b6c776f5860d3847
axdevil <deb.devel@outlook.es>
2024-01-17 18:43:08 -0600

    Cambio de visibilidad de formularios

--- client/src/app/components/sign-up/sign-up.component.ts
+++ client/src/app/components/sign-up/sign-up.component.ts
@@ -4,12 +4,12 @@ import { UsernameComponent } from './username/username.component';
 import { LogoComponent } from '../logo/logo.component';
 
 @Component({
-  selector: 'app-sign-up',
-  standalone: true,
-  imports: [BasicComponent, UsernameComponent, LogoComponent],
-  templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+    selector: 'app-sign-up',
+    standalone: true,
+    imports: [BasicComponent, UsernameComponent, LogoComponent],
+    templateUrl: './sign-up.component.html',
+    styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
-
+    numForm = 0
 }

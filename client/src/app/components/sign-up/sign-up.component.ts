e6914e1438f42aa2c3d77271a3eccf1e35207276
axdevil <deb.devel@outlook.es>
2024-01-18 23:21:18 -0600

    Previo guardado de datos

--- client/src/app/components/sign-up/sign-up.component.ts
+++ client/src/app/components/sign-up/sign-up.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BasicComponent } from './basic/basic.component';
 import { UsernameComponent } from './username/username.component';
 import { LogoComponent } from '../logo/logo.component';
+import { User } from '../../interfaces/user';
 
 @Component({
     selector: 'app-sign-up',
@@ -11,12 +12,36 @@ import { LogoComponent } from '../logo/logo.component';
     styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
-    numForm = 1;
+    numForm = 0;
+
+    newUser: User = {
+        username: '',
+        email: '',
+        password: '',
+        picture: 'default.jpg',
+    };
 
     nextForm(): void {
         this.numForm += 1;
+
+        if (this.numForm > 1) {
+            alert('Save user in DB');
+        }
     }
     previousForm(): void {
         this.numForm -= 1;
+    }
+
+    receiveBasicData(data: string[]) {
+        this.newUser.username = data[0];
+        this.newUser.email = data[1];
+
+        this.nextForm();
+    }
+
+    receiveUsername(username: string) {
+        this.newUser.username = username;
+
+        this.nextForm();
     }
 }

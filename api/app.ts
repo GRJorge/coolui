cf9fd85ce326d498626ebc99d97ba3c608548011
axdevil <deb.devel@outlook.es>
2024-01-19 18:33:04 -0600

    Integracion de CORS en api

--- api/app.ts
+++ api/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 import 'dotenv/config';
 import './config/db';
 /* IMPORTACION DE RUTAS */
@@ -8,6 +9,7 @@ import userRouter from './routes/user';
 
 const app = express();
 
+app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

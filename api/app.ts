c4239e93916c3bbb99f079e2adf84275ad9dd589
axdevil <deb.devel@outlook.es>
2024-01-21 00:21:55 -0600

    Configuracion de express-session

--- api/app.ts
+++ api/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import cors from 'cors';
+import session from 'express-session';
+import MongoDBStoreModule from 'connect-mongodb-session';
 import 'dotenv/config';
 import './config/db';
 /* IMPORTACION DE RUTAS */
@@ -9,7 +11,28 @@ import userRouter from './routes/user';
 
 const app = express();
 
-app.use(cors())
+const mongoDBStore = MongoDBStoreModule(session);
+const store = new mongoDBStore({
+    uri: (process.env.DB_URL + process.env.DB_NAME!),
+    collection: 'sessions'
+})
+
+store.on('error', (error: Error) => {
+    console.error(error);
+});
+
+app.use(session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    store,
+    cookie: {
+        secure: false,
+        maxAge: 1 * 24 * 60 * 60 * 1000
+    }
+}))
+
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
